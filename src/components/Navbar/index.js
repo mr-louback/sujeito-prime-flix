f29137fb5a96f00961b9d04a8c73f719a0c25e5b
mr-louback <loubacked4work@outlook.com>
2023-11-02 23:59:28 -0300

    icones

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,22 @@
 import { Link } from 'react-router-dom';
+import {
+  BiHomeAlt2,
+  BiSolidContact
+} from 'react-icons/bi';
+import {
+  SiAboutdotme
+} from 'react-icons/si';
 import './style.css';
 
 function Navbar() {
   return (
     <>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
+      <h1>LogoMarca</h1>
+      <div>
+        <Link to="/"><BiHomeAlt2 /></Link>
+        <Link to="/about"><SiAboutdotme /></Link>
+        <Link to="/contact"><BiSolidContact /></Link>
+      </div>
     </>
   )
 }
